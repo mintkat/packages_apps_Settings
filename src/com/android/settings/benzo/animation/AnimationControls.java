248a492bef9cc631a3aff1d2e4182e2f0b4d7e93
cristianomatos <cristianobmatos@gmail.com>
2014-12-29 15:58:29 -0200

    AOKP animations: Add and entry for TRANSIT_TASK_OPEN_BEHIND
    Already in frameworks/base. Option was just missing from Settings

--- src/com/android/settings/benzo/animation/AnimationControls.java
+++ src/com/android/settings/benzo/animation/AnimationControls.java
@@ -51,6 +51,7 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
     private static final String WALLPAPER_CLOSE = "wallpaper_close";
     private static final String WALLPAPER_INTRA_OPEN = "wallpaper_intra_open";
     private static final String WALLPAPER_INTRA_CLOSE = "wallpaper_intra_close";
+    private static final String TASK_OPEN_BEHIND = "task_open_behind";
 
     ListPreference mActivityOpenPref;
     ListPreference mActivityClosePref;
@@ -62,6 +63,7 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
     ListPreference mWallpaperClose;
     ListPreference mWallpaperIntraOpen;
     ListPreference mWallpaperIntraClose;
+    ListPreference mTaskOpenBehind;
     SlimSeekBarPreference mAnimationDuration;
     SwitchPreference mAnimNoOverride;
 
@@ -155,6 +157,12 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
         mWallpaperIntraClose.setEntries(mAnimationsStrings);
         mWallpaperIntraClose.setEntryValues(mAnimationsNum);
 
+        mTaskOpenBehind = (ListPreference) findPreference(TASK_OPEN_BEHIND);
+        mTaskOpenBehind.setOnPreferenceChangeListener(this);
+        mTaskOpenBehind.setSummary(getProperSummary(mTaskOpenBehind));
+        mTaskOpenBehind.setEntries(mAnimationsStrings);
+        mTaskOpenBehind.setEntryValues(mAnimationsNum);
+
         mAnimationDuration = (SlimSeekBarPreference) findPreference(ANIMATION_DURATION);
         mAnimationDuration.setDefault(0);
         mAnimationDuration.isMilliseconds(true);
@@ -222,6 +230,10 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
             int val = Integer.parseInt((String) newValue);
             result = Settings.System.putInt(mContentRes,
                     Settings.System.ACTIVITY_ANIMATION_CONTROLS[9], val);
+        } else if (preference == mTaskOpenBehind) {
+            int val = Integer.parseInt((String) newValue);
+            result = Settings.System.putInt(mContentRes,
+                    Settings.System.ACTIVITY_ANIMATION_CONTROLS[10], val);
         } else if (preference == mAnimationDuration) {
             int val = Integer.parseInt((String) newValue);
             Settings.System.putInt(mContentRes,
@@ -253,6 +265,8 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
             mString = Settings.System.ACTIVITY_ANIMATION_CONTROLS[8];
         } else if (preference == mWallpaperIntraClose) {
             mString = Settings.System.ACTIVITY_ANIMATION_CONTROLS[9];
+        } else if (preference == mTaskOpenBehind) {
+            mString = Settings.System.ACTIVITY_ANIMATION_CONTROLS[10];
         }
 
         int mNum = Settings.System.getInt(mContentRes, mString, 0);
