f080c87d190ce594ad676f411accd350303dfeb1
Randall Rushing <randall.rushing@gmail.com>
2015-07-24 00:49:41 -0700

    QSTiles: Set number of tile columns (2 of 2)
    3, 4, or 5 columns. We don't want to get excessive here
    
    PROBLEM: When enlarged first 2 tiles is enabled, the grid
    view first row always shows COL_COUNT - 1 tiles, even though
    the max number is two. Some crews have decided to tolerate this
    but I can not. At the same time, a solid solution to effectively
    measure the specs of the layout will require a total overhaul of
    the DraggableGridView class. This is also not acceptable
    at this time. What to do?
    
    SOLUTION: We disable DraggableGridView from reflecting the enlarged
    tile state. So if rows per tile is set to 4, DraggableGridView will
    show just that, regardless of enlarged tile state. This has no
    impact on the actual presentation in QSPanel. We simply don't treat
    enlarged tiles as a special case. This is not the most elegant of
    solutions. However, it is my assessment it is a reasonable compromise
    in an effort to bring this killer enhancement to mainline.
    
    Change-Id: Iab7e4266600a059b96d867c58f35d92005659060

--- src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2015 The TeamEos Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +31,7 @@ import android.os.Vibrator;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.cyanogenmod.qs.DraggableGridView;
 import com.android.settings.cyanogenmod.qs.QSTiles;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
@@ -51,6 +53,7 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
     private SwitchPreference mBlockOnSecureKeyguard;
     private SwitchPreference mQsVibrate;
     private Preference mQSTiles;
+    private ListPreference mNumColumns;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -92,6 +95,16 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
         if (!lockPatternUtils.isSecure()) {
             prefSet.removePreference(mBlockOnSecureKeyguard);
         }
+
+        mNumColumns = (ListPreference) findPreference("sysui_qs_num_columns");
+        int numColumns = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.QS_NUM_TILE_COLUMNS, getDefaultNumColums(),
+                UserHandle.USER_CURRENT);
+        mNumColumns.setValue(String.valueOf(numColumns));
+        updateNumColumnsSummary(numColumns);
+        mNumColumns.setOnPreferenceChangeListener(this);
+        DraggableGridView.setColumnCount(numColumns);
+
     }
 
     @Override
@@ -120,8 +133,34 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
                     smartPulldownValue, UserHandle.USER_CURRENT);
             mSmartPulldown.setSummary(mSmartPulldown.getEntries()[index]);
             return true;
+        } else if (preference == mNumColumns) {
+            int numColumns = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.QS_NUM_TILE_COLUMNS,
+                    numColumns, UserHandle.USER_CURRENT);
+            updateNumColumnsSummary(numColumns);
+            DraggableGridView.setColumnCount(numColumns);
+            return true;
         }
         return false;
+    }
+
+    private void updateNumColumnsSummary(int numColumns) {
+        String prefix = (String) mNumColumns.getEntries()[mNumColumns.findIndexOfValue(String
+                .valueOf(numColumns))];
+        mNumColumns.setSummary(getResources().getString(R.string.qs_num_columns_showing, prefix));
+    }
+
+    private int getDefaultNumColums() {
+        try {
+            Resources res = getPackageManager()
+                    .getResourcesForApplication("com.android.systemui");
+            int val = res.getInteger(res.getIdentifier("quick_settings_num_columns", "integer",
+                    "com.android.systemui")); // better not be larger than 5, that's as high as the
+                                              // list goes atm
+            return Math.max(1, val);
+        } catch (Exception e) {
+            return 3;
+        }
     }
 
     public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
