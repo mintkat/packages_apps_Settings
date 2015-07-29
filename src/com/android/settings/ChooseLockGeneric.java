TeamBliss-LP/android_packages_apps_Settings

Settings: Gesture Lockscreen (2/2) Lets give this a go again.
Changes needed for lollipop

AICPfy

Change-Id: Icf2f5a5cd3474ea325097437bc2956c10e2ae9d7

Conflicts:
	res/values/bliss_attrs.xml
	res/values/bliss_strings.xml

src/com/android/settings/ChooseLockGeneric.java
@@ -72,6 +72,7 @@ public class ChooseLockGeneric extends SettingsActivity {
         private static final String KEY_UNLOCK_SET_PIN = "unlock_set_pin";
         private static final String KEY_UNLOCK_SET_PASSWORD = "unlock_set_password";
         private static final String KEY_UNLOCK_SET_PATTERN = "unlock_set_pattern";
+        private static final String KEY_UNLOCK_SET_GESTURE = "unlock_set_gesture";
         private static final int CONFIRM_EXISTING_REQUEST = 100;
         private static final int FALLBACK_REQUEST = 101;
         private static final int ENABLE_ENCRYPTION_REQUEST = 102;
@@ -170,6 +171,9 @@ public class ChooseLockGeneric extends SettingsActivity {
             } else if (KEY_UNLOCK_SET_PASSWORD.equals(key)) {
                 maybeEnableEncryption(
                         DevicePolicyManager.PASSWORD_QUALITY_ALPHABETIC, false);
+            } else if (KEY_UNLOCK_SET_GESTURE.equals(key)) {
+                updateUnlockMethodAndFinish(
+                        DevicePolicyManager.PASSWORD_QUALITY_GESTURE_WEAK, false);
             } else {
                 handled = false;
             }
@@ -419,7 +423,20 @@ public class ChooseLockGeneric extends SettingsActivity {
 
             quality = upgradeQuality(quality, null);
 
-            if (quality >= DevicePolicyManager.PASSWORD_QUALITY_NUMERIC) {
+            if (quality == DevicePolicyManager.PASSWORD_QUALITY_GESTURE_WEAK) {
+                Intent intent = new Intent().setClass(getActivity(), ChooseLockGesture.class);
+                intent.putExtra(CONFIRM_CREDENTIALS, false);
+                intent.putExtra(LockPatternUtils.LOCKSCREEN_BIOMETRIC_WEAK_FALLBACK,
+                        isFallback);
+                if (isFallback) {
+                    startActivityForResult(intent, FALLBACK_REQUEST);
+                    return;
+                } else {
+                    mFinishPending = true;
+                    intent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
+                    startActivity(intent);
+                }
+            } else if (quality >= DevicePolicyManager.PASSWORD_QUALITY_NUMERIC) {
                 int minLength = mDPM.getPasswordMinimumLength(null);
                 if (minLength < MIN_PASSWORD_LENGTH) {
                     minLength = MIN_PASSWORD_LENGTH;

mintkat/packages_apps_Settings

src/com/android/settings/ChooseLockGeneric.java
@@ -80,6 +80,7 @@ public class ChooseLockGeneric extends SettingsActivity {
         private static final String KEY_UNLOCK_SET_PIN = "unlock_set_pin";
         private static final String KEY_UNLOCK_SET_PASSWORD = "unlock_set_password";
         private static final String KEY_UNLOCK_SET_PATTERN = "unlock_set_pattern";
+        private static final String KEY_UNLOCK_SET_GESTURE = "unlock_set_gesture";
         private static final int CONFIRM_EXISTING_REQUEST = 100;
         private static final int FALLBACK_REQUEST = 101;
         private static final int ENABLE_ENCRYPTION_REQUEST = 102;
@@ -450,7 +451,20 @@ public class ChooseLockGeneric extends SettingsActivity {
             quality = upgradeQuality(quality, null);
 
             final Context context = getActivity();
-            if (quality >= DevicePolicyManager.PASSWORD_QUALITY_NUMERIC) {
+            if (quality == DevicePolicyManager.PASSWORD_QUALITY_GESTURE_WEAK) {
+                Intent intent = new Intent().setClass(getActivity(), ChooseLockGesture.class);
+                intent.putExtra(CONFIRM_CREDENTIALS, false);
+                intent.putExtra(LockPatternUtils.LOCKSCREEN_BIOMETRIC_WEAK_FALLBACK,
+                        isFallback);
+                if (isFallback) {
+                    startActivityForResult(intent, FALLBACK_REQUEST);
+                    return;
+                } else {
+                    mFinishPending = true;
+                    intent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
+                    startActivity(intent);
+                }
+            } else if (quality >= DevicePolicyManager.PASSWORD_QUALITY_NUMERIC) {
                 int minLength = mDPM.getPasswordMinimumLength(null);
                 if (minLength < MIN_PASSWORD_LENGTH) {
                     minLength = MIN_PASSWORD_LENGTH;
@@ -538,6 +552,9 @@ public class ChooseLockGeneric extends SettingsActivity {
             } else if (KEY_UNLOCK_SET_PASSWORD.equals(unlockMethod)) {
                 maybeEnableEncryption(
                         DevicePolicyManager.PASSWORD_QUALITY_ALPHABETIC, false);
+            } else if (KEY_UNLOCK_SET_GESTURE.equals(unlockMethod)) {
+                updateUnlockMethodAndFinish(
+                        DevicePolicyManager.PASSWORD_QUALITY_GESTURE_WEAK, false);
             } else {
                 Log.e(TAG, "Encountered unknown unlock method to set: " + unlockMethod);
                 return false;