22060f73b58d142c48d3d089eb142591aafe7bf7
zephiK <zephik@gmail.com>
2015-10-11 11:33:59 -0400

    Longpress back to kill app [2/2]
    Part two of https://github.com/zephiK/android_frameworks_base/commit/132a7a7e80beaf3ffa4c6428b30c2349569ed8d0
    Originating commit from rascarlo - https://github.com/rascarlo/android_frameworks_base/commit/cc7cbd4c359bb88d0f482ca19b1c2285643cbe71
    
    Change-Id: I7693f33a6e4dc7d04a12a1051e5b7cbd65b8203d

--- src/com/android/settings/benzo/NavbarSettings.java
+++ src/com/android/settings/benzo/NavbarSettings.java
@@ -16,11 +16,18 @@
 package com.android.settings.benzo;
 
 import android.os.Bundle;
+import android.preference.SwitchPreference;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
 
-public class NavbarSettings extends SettingsPreferenceFragment {
+public class NavbarSettings extends SettingsPreferenceFragment
+        OnPreferenceChangeListener {
+
+    private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+
+    private SwitchPreference mKillAppLongPressBack;
 
     @Override
     protected int getMetricsCategory() {
@@ -31,5 +38,22 @@ public class NavbarSettings extends SettingsPreferenceFragment {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.navbar_settings);
+
+        // kill-app long press back
+        mKillAppLongPressBack = (SwitchPreference) findPreference(KILL_APP_LONGPRESS_BACK);
+        mKillAppLongPressBack.setOnPreferenceChangeListener(this);
+        int killAppLongPressBack = Settings.Secure.getInt(getContentResolver(),
+                KILL_APP_LONGPRESS_BACK, 0);
+        mKillAppLongPressBack.setChecked(killAppLongPressBack != 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mKillAppLongPressBack) {
+            boolean value = (Boolean) objValue;
+            Settings.Secure.putInt(getContentResolver(), KILL_APP_LONGPRESS_BACK,
+                    value ? 1 : 0);
+        }
+            return true;
     }
 }
